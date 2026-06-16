026abd544fb34ff9f02eae7974c780cb5b3e5ba7
chijioke-coder <chijiokegideon54@gmail.com>
2026-06-16 19:44:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015', // Transpile optional chaining and other modern syntax
+    outDir: 'dist',
+  },
   server: {
     port: 3000,
-    host: true
-  }
+  },
 })
